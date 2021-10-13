cecadb2861074321eec691d145484209a07eaa42
Caleb Felix <calebfelix38@gmail.com>
2021-10-13 16:19:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require('axios');
+
+const app = express();
+const apiKey = "87de4b3eba4d7d554106c0b0d2a32dc0"
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+app.get("/",  (req, res) => {
+    res.render("index", { data: null, error: null });
+  });
+
+  app.post("/", async (req, res) => {
+     const loc = req.body.Location
+try{
+     const url = `https://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${apiKey}`;
+     const data = await axios.get(url);
+     console.log(data.data)
+     res.render("index", { data : data.data, error: null });
+    } catch(err){
+        res.render("index", { data : null, error: err });
+    }
+  });
+
+app.listen(5000 || process.env.PORT, function () {
+    console.log("Weather app listening on port 5000!");
+  });
